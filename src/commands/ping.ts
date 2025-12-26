969097b954937e13908c47364753823861f19ca6
tanahiro2010 <herentongkegu087@gmail.com>
2025-12-26 14:33:24 +0900

    pingコマンドを新規作成し、WebSocketのPingを返す機能を追加

---
+++ src/commands/ping.ts
@@ -0,0 +1,14 @@
+import { CommandInteraction, MessageFlags } from "discord.js";
+import { Command } from "../types/command";
+
+module.exports = {
+    data: {
+        name: "ping",
+        flags: MessageFlags.Ephemeral,
+        description: "Ping command"
+    },
+    async execute(interaction: CommandInteraction): Promise<void> {
+        await interaction.followUp(`WebSocket Ping: ${interaction.client.ws.ping}ms`);
+        return;
+    }
+} as Command;
