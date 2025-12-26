7c07cbbeb6059f59bc31ba20f42e13216dced10c
tanahiro2010 <herentongkegu087@gmail.com>
2025-12-26 18:22:58 +0900

    customIdの型を変更し、JSON.stringifyで変換するように修正

--- src/libs/button.ts
+++ src/libs/button.ts
@@ -2,14 +2,14 @@ import { ButtonBuilder, ButtonStyle } from "discord.js"
 
 type Props = {
     label: string;
-    customId: string;
+    customId: Record<string, any> | string;
     style?: ButtonStyle;
 }
 
 const createButton = (props: Props) => {
     return new ButtonBuilder()
         .setLabel(props.label)
-        .setCustomId(props.customId)
+        .setCustomId(JSON.stringify(props.customId))
         .setStyle(props.style ?? ButtonStyle.Primary);
 }
 
